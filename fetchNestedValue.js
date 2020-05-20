e3f4783dac848bd625ea55df1571842aa045edc9
Eugene Cheah <eugeneqin@gmail.com>
2020-05-20 14:11:17 +0000

    Initial implementation

---
+++ fetchNestedValue.js
@@ -0,0 +1,34 @@
+/**
+ * Given an object and fetch
+ * @param {Object} obj 
+ * @param {Array<String> | String} path
+ * @param {Object} fallback value
+ */
+function fetchNestedValue(obj, path, fallback = null) {
+	// Convert path to array
+	if(!Array.isArray(path)) {
+		return fetchNestedValue(obj, path.toString().split("."));
+	}
+
+	// Get the first element, and remove it from the path array
+	const key = path.shift();
+
+	// Get the key value
+	const val = obj[key];
+
+	// If value is null, terminate and return fallback
+	if( val === null ) {
+		return fallback;
+	}
+
+	// Recusively call if needed
+	if( path.length > 0 ) {
+		return fetchNestedValue(val, path);
+	}
+
+	// Return the value
+	return val;
+}
+
+// Export the function
+module.exports = fetchNestedValue;
