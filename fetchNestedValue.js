500fbe48eeab9b7939d43d93b8ac94b19a93eb99
Eugene Cheah <eugeneqin@gmail.com>
2020-09-24 15:55:50 +0000

    Bug Fixing

--- fetchNestedValue.js
+++ fetchNestedValue.js
@@ -12,7 +12,7 @@ function fetchNestedValue(obj, path, fallback = null) {
 	
 	// Convert path to array
 	if(!Array.isArray(path)) {
-		return fetchNestedValue(obj, path.toString().split("."));
+		path = path.toString().split(".");
 	}
 
 	// Get the first element, and remove it from the path array
@@ -22,13 +22,13 @@ function fetchNestedValue(obj, path, fallback = null) {
 	const val = obj[key];
 
 	// If value is null, terminate and return fallback
-	if( val === null ) {
+	if( val === null || val === undefined ) {
 		return fallback;
 	}
 
 	// Recusively call if needed
 	if( path.length > 0 ) {
-		return fetchNestedValue(val, path);
+		return fetchNestedValue(val, path, fallback);
 	}
 
 	// Return the value
